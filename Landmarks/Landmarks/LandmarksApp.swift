1194ae8366028c0eb70f91288337f2ecd8246db8
oksunwoo <oksunwo@naver.com>
2022-08-15 20:49:19 +0900

    initial commit

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sunwoo on 2022/08/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
